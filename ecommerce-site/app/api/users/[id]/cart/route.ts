b4111c7569a50f870ca922d50039762051ed84cd
ellysiahime <ellysiahime@gmail.com>
2025-06-24 08:49:02 +0000

    shopping cart api, connect with mongodb

--- ecommerce-site/app/api/users/[id]/cart/route.ts
+++ ecommerce-site/app/api/users/[id]/cart/route.ts
@@ -31,3 +31,38 @@ export async function GET(request: NextRequest, { params }: { params: Params }) 
     },
   });
 }
+
+type CartBody = {productId: string};
+
+export async function POST( request: NextRequest, { params }: { params: Params }) {
+  const userId = params.id;
+  const body: CartBody = await request.json();
+  const productId = body.productId;
+
+  carts[userId] = carts[userId] ? carts[userId].concat(productId) : [productId];
+  const cartProducts = carts[userId].map(id => products.find(p => p.id === id));
+
+  return new Response(JSON.stringify(cartProducts), {
+    status: 201,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+}
+
+export async function DELETE(request: NextRequest, { params }: { params: Params }) {
+  const userId = params.id;
+  const body = await request.json();
+  const productId = body.productId;
+
+  carts[userId] = carts[userId] ? carts[userId].filter(pid => pid !== productId) : [];
+  const cartProducts = carts[userId].map(id => products.find(p => p.id === id));
+
+  return new Response(JSON.stringify(cartProducts), {
+    status: 202,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
